ff87cc11131c64c85a52eea8ce00f13651198b18
Damian Suess <djsuess@gmail.com>
2021-08-10 14:53:42 -0400

    Base Prism Uno app from template

---
+++ Ex9-PrismUno/PrismUno.Core/Mvvm/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using Prism.Mvvm;
+
+namespace PrismUno.Core.Mvvm
+{
+  public class ViewModelBase : BindableBase
+  {
+    private string _title;
+    public string Title
+    {
+      get { return _title; }
+      set { SetProperty(ref _title, value); }
+    }
+  }
+}
---
+++ Ex9-PrismUno/PrismUno.Shared/ViewModels/ShellViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using PrismUno.Core.Mvvm;
+using System;
+
+namespace PrismUno.Shared.ViewModels
+{
+  public class ShellViewModel : ViewModelBase
+  {
+    public ShellViewModel()
+    {
+      Title = "Main Page";
+    }
+  }
+}
