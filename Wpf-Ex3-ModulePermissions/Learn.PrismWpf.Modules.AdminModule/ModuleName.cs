42b0c6d73bb2d419207c9d854b8a2d1ea3ad313c
Damian Suess <djsuess@gmail.com>
2021-10-14 14:29:17 -0400

    Added beginning sample of Module Permissions

---
+++ Wpf-Ex3-ModulePermissions/Learn.PrismWpf.Modules.AdminModule/ModuleName.cs
@@ -0,0 +1,20 @@
+﻿using Learn.PrismWpf.Modules.AdminModule.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Learn.PrismWpf.Modules.AdminModule
+{
+  public class ModuleName : IModule
+  {
+    public void OnInitialized(IContainerProvider containerProvider)
+    {
+
+    }
+
+    public void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+
+    }
+  }
+}
