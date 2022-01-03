16fec825baa0b16648b8db05e9f24769d69db1d0
Damian Suess <djsuess@gmail.com>
2022-01-03 18:23:27 -0500

    basis for GTK navigation

---
+++ Xamarin-Ex10-Gtk-Navigation/Sample.PrismMobile/ViewModels/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Commands;
+using Prism.Navigation;
+
+namespace Sample.PrismMobile.ViewModels
+{
+  public class MainViewModel : ViewModelBase
+  {
+    private int _counter = 0;
+    private string _message;
+
+    public MainViewModel(INavigationService navigationService)
+      : base(navigationService)
+    {
+      Title = "Main Page";
+      Message = "Welcome to Xamarin Forms and Prism!";
+    }
+
+    public string Message
+    {
+      get => _message;
+      set => SetProperty(ref _message, value);
+    }
+
+    public DelegateCommand CmdTestButton => new DelegateCommand(() =>
+    {
+      _counter++;
+      Message = $"Button Clicked {_counter} Times!";
+    });
+  }
+}
---
+++ Xamarin-Ex10-Gtk-Navigation/readme.md
@@ -0,0 +1,12 @@
+# Prism.Forms - GTK Intro
+
+## Overview
+
+Sample navigation Prism with Xamarin GTK
+
+## References
+
+* [Prism GTK](https://prismlibrary.com/docs/xamarin-forms/platform-reference/gtk.html)
+* [Prism Library](https://github.com/PrismLibrary/Prism)
+* [Xamarin.Forms](https://github.com/xamarin/Xamarin.Forms)
+* [GTK# Platform Setup](https://docs.microsoft.com/en-us/xamarin/xamarin-forms/platform/other/gtk?tabs=windows)
