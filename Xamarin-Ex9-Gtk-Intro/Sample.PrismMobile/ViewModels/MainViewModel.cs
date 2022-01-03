f4db8bbe20d657e4011daff61b672e9cc55e5882
Damian Suess <djsuess@gmail.com>
2022-01-03 18:04:04 -0500

    GTK simple binding example

--- Xamarin-Ex9-Gtk-Intro/Sample.PrismMobile/ViewModels/MainViewModel.cs
+++ Xamarin-Ex9-Gtk-Intro/Sample.PrismMobile/ViewModels/MainViewModel.cs
@@ -1,27 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Prism.Commands;
-using Prism.Mvvm;
+﻿using Prism.Commands;
 using Prism.Navigation;
 
 namespace Sample.PrismMobile.ViewModels
 {
   public class MainViewModel : ViewModelBase
   {
+    private int _counter = 0;
+    private string _message;
+
     public MainViewModel(INavigationService navigationService)
-        : base(navigationService)
+      : base(navigationService)
     {
       Title = "Main Page";
+      Message = "Welcome to Xamarin Forms and Prism!";
     }
 
-    public DelegateCommand CmdTestButton => new DelegateCommand(async () =>
+    public string Message
     {
-      await Task.Yield();
+      get => _message;
+      set => SetProperty(ref _message, value);
+    }
+
+    public DelegateCommand CmdTestButton => new DelegateCommand(() =>
+    {
+      _counter++;
+      Message = $"Button Clicked {_counter} Times!";
     });
-
-
   }
 }
