6f686a806d517c9a1336bb5c27f0547f60a58604
Damian Suess <djsuess@gmail.com>
2022-07-08 14:00:15 -0400

    Prism.Maui SplashScreen - Added base project. Moved output folders for Maui projects for easy deleting

---
+++ Maui-Ex3-SplashScreen/Test.PrismMaui/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Test.PrismMaui.Views;
+
+namespace Test.PrismMaui.ViewModels
+{
+  public class MainPageViewModel : BindableBase
+  {
+    private readonly INavigationService _nav;
+    private ISemanticScreenReader _screenReader { get; }
+    private int _counter;
+    private string _text;
+
+    public MainPageViewModel(ISemanticScreenReader screenReader, INavigationService nav)
+    {
+      _screenReader = screenReader;
+      _nav = nav;
+      Text = "Click Me!";
+    }
+
+    public string Title => "Prism Maui - Intro";
+
+    public DelegateCommand CmdCounter => new DelegateCommand(OnCounter);
+
+    public DelegateCommand CmdNavigate => new DelegateCommand(() =>
+    {
+      string navTo = $"{nameof(NavigationPage)}/{nameof(MainPage)}/{nameof(Page2View)}";
+      _nav.NavigateAsync(navTo);
+    });
+
+    public string Text
+    {
+      get => _text;
+      set => SetProperty(ref _text, value);
+    }
+
+    private void OnCounter()
+    {
+      _counter++;
+
+      if (_counter == 1)
+        Text = "Clicked one time";
+      else
+        Text = $"Clicked {_counter} times";
+
+      // Update accessability screen reader. Ref: https://docs.microsoft.com/en-us/dotnet/maui/fundamentals/accessibility
+      _screenReader.Announce(Text);
+    }
+  }
+}
---
+++ Maui-Ex3-SplashScreen/readme.md
@@ -0,0 +1,13 @@
+# Prism.Maui - Splash Screen
+
+## Overview
+
+In this example we'll perform a simple splash screen from the Maui-level, not Android/WinUI/etc front like traditional Xamarin.Forms apps did.
+
+This allows you to reuse code and provide live-loading screens to the users.
+
+## References
+
+* [Prism.Maui](https://github.com/dansiegel/Prism.Maui)
+* [Prism Library](https://github.com/PrismLibrary/Prism)
+* [Xamarin.Forms](https://github.com/xamarin/Xamarin.Forms)
