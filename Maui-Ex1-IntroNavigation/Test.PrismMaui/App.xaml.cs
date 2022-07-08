e6be932deb57b6f988cf9cbc8af633f378dad401
Damian Suess <djsuess@gmail.com>
2022-07-08 12:16:48 -0400

    Added base Prism application

---
+++ Maui-Ex1-IntroNavigation/Test.PrismMaui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Test.PrismMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Maui-Ex1-IntroNavigation/Test.PrismMaui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Test.PrismMaui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
