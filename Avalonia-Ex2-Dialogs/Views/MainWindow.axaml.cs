d04b8e1d72c400cf48565d2f503e05e5d2abac72
Damian Suess <djsuess@gmail.com>
2023-08-25 12:49:00 -0400

    Added Avalonia Dialog Sample

---
+++ Avalonia-Ex2-Dialogs/Views/MainWindow.axaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace SampleDialogApp.Views
+{
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+#if DEBUG
+      this.AttachDevTools();
+#endif
+    }
+
+    // When referencing Avalonia package, XamlNameReferenceGenerator
+    ////private void InitializeComponent()
+    ////{
+    ////    AvaloniaXamlLoader.Load(this);
+    ////}
+  }
+}
