9a7fa037e60cd0029b93521f9a4fb6910382665d
Damian Suess <djsuess@gmail.com>
2024-08-03 23:44:11 -0400

    Base Avalonia UI Tester sample app; prepping for Verify.Avalonia

---
+++ Avalonia-Ex4-UITester/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SampleUITester.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+  public MainWindowViewModel()
+  {
+    Title = "Welcome to Prism.Avalonia by Suess Labs!";
+  }
+
+  public string Greeting => "Welcome to Prism.Avalonia!";
+}
---
+++ Avalonia-Ex4-UITester/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SampleUITester.Views
+{
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
