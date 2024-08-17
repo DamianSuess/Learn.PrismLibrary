f9968955bdf693c817673f1fb27803fe25696cfa
Damian Suess <djsuess@gmail.com>
2024-08-17 09:15:14 -0400

    base project

---
+++ MAUI/Maui-Ex7-DialogPopups/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Sample.DialogPopups
+{
+  public partial class App : Application
+  {
+    public App()
+    {
+      InitializeComponent();
+
+      MainPage = new AppShell();
+    }
+  }
+}
---
+++ MAUI/Maui-Ex7-DialogPopups/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Sample.DialogPopups;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+  protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
