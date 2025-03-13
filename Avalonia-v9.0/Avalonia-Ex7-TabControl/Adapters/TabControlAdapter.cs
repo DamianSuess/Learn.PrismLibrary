fceb9ddf03f89340c8975a3b25fb44a099074510
Damian Suess <djsuess@gmail.com>
2025-03-13 15:37:43 -0400

    base app for tab control

---
+++ Avalonia-v9.0/Avalonia-Ex7-TabControl/Adapters/TabControlAdapter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avalonia-Ex7-TabControl.Adapters
+{
+    internal class TabControlAdapter
+{
+}
+}
---
+++ Avalonia-v9.0/Avalonia-Ex7-TabControl/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SampleApp.Views
+{
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
