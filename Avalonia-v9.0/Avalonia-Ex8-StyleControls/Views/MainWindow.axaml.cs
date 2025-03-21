05509a9e6bb77a60830ecabb6d6f7da256f9958e
Damian Suess <djsuess@gmail.com>
2025-03-21 10:31:27 -0400

    base app

---
+++ Avalonia-v9.0/Avalonia-Ex8-StyleControls/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Controls;
+
+namespace SampleApp.Views
+{
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
